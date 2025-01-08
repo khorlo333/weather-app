60345de7fb7929cdaaae649bc424b00586acf875
khorlo333 <24lp3394@gmail.com>
2025-01-08 09:39:59 +0800

    city name shown

--- src/components/Circles.jsx
+++ src/components/Circles.jsx
@@ -3,16 +3,6 @@ export default function Circles() {
     <div>
       <div className=" w-[1340px] h-[1340px] absolute rounded-full border border-muted-foreground/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
 
-      <img
-        className="w-[150px] h-[150px] absolute left-[250px] top-[100px] z-1"
-        src="sun.svg"
-        alt=""
-      />
-      <img
-        className="w-[150px] h-[150px] absolute right-[250px] bottom-[100px] z-1"
-        src="ellipse.svg"
-        alt=""
-      />
       <div className=" w-[940px] h-[940px] absolute rounded-full border border-muted-foreground/20 z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className=" w-[540px] h-[540px] absolute rounded-full border border-muted-foreground/20 z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className=" w-[340px] h-[340px] absolute rounded-full border border-muted-foreground/20 z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
--- src/components/RightData.jsx
+++ src/components/RightData.jsx
@@ -1,14 +1,19 @@
 export default function RightCard() {
   return (
     <div className="flex justify-between items-center flex-col w-[400px] h-[700px] relative z-10 rounded-t-md ">
-      <div className="flex justify-around items-center flex-col w-[400px] h-[490px] absolute top-0 z-10 bg-[#00]/40 backdrop-blur-md rounded-t-md">
+      <div className="flex justify-around items-center flex-col w-[400px] h-[490px] absolute top-0 z-10 bg-[#111827]/50 backdrop-blur-md rounded-t-md">
         <div className="p-1 pl-9 self-start ">
-          <p className="text-black/20">Jan 7, 2025</p>
-          <h1 className="text-[45px] font">Tokyo</h1>
+          <p className="text-white/20">Jan 7, 2025</p>
+          <h1 className="text-[45px] text-white">Tokyo</h1>
         </div>
         <img className=" w-[250px] h-[250px]" src="Moon.png" alt="" />
       </div>
-      <div className="flex justify-center items-center flex-col w-[400px] h-[210px] absolute bottom-0 z-10 bg-[#00]/90 backdrop-blur-sm rounded-b-md"></div>
+      <div className="flex justify-center items-center flex-col w-[400px] h-[210px] absolute bottom-0 z-10 bg-[#111827]/30 backdrop-blur-sm rounded-b-md"></div>
+      <img
+        className="w-[150px] h-[150px] absolute right-[-75px] bottom-[-75px] z-1"
+        src="ellipse.svg"
+        alt=""
+      />
     </div>
   );
 }
